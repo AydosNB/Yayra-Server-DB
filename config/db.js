abc5d09a4bbd07c2cd8d4969a62a5ac32926ef99
AydosNB <nawrizbaevaydos50@gmail.com>
2025-01-15 20:41:05 +0500

    sasd

--- config/db.js
+++ config/db.js
@@ -3,9 +3,13 @@ import mongoose from "mongoose";
 
 export const connectMongoDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
         console.log(`MongoDB connected: ${conn.connection.host}`)
     } catch (error) {
         console.error(`Error : ${error}`)
+        process.exit(1);
     }
 }
